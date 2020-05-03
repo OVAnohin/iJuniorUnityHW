d68b08c77a5a39894ef825067b41c62892647187
Oleg <oanohin@outlook.com>
2020-05-03 21:57:34 +0400

    HW6 Генерация врагов на уровне

---
+++ HW6/Assets/Scripts/EvilSunMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class EvilSunMove : MonoBehaviour
+{
+  [SerializeField] private float _speed = 0;
+  [SerializeField] private Transform _groundCheck = null;
+  [SerializeField] private float _checkRadius = 0.5f;
+  [SerializeField] private LayerMask _whatIsGround = new LayerMask();
+
+  private Rigidbody2D _rigidbody2D;
+  private System.Random rand = new System.Random();
+  private int _direction;
+  private Collider2D _isGrounded;
+
+  private void Start()
+  {
+    _rigidbody2D = GetComponent<Rigidbody2D>();
+    _direction = rand.Next(2) == 1 ? 1 : -1;
+  }
+
+  private void FixedUpdate()
+  {
+    _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _checkRadius, _whatIsGround);
+
+    if (_isGrounded)
+    {
+      Vector2 moveInput = new Vector2(_direction, 0);
+      Vector2 _moveVelocity = moveInput * _speed;
+      _rigidbody2D.MovePosition(_rigidbody2D.position + _moveVelocity * Time.fixedDeltaTime);
+    }
+  }
+}
---
+++ HW6/Assets/Scripts/Platform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+  [SerializeField] private EvilSun _evilSun = null;
+  [SerializeField] float _timeStart = 0;
+
+  private EvilSun _evilSunClone;
+
+  void Start()
+  {
+    InvokeRepeating("GenerateEnemy", _timeStart, 2.0f);
+  }
+
+  private void GenerateEnemy()
+  {
+    _evilSunClone = Instantiate(_evilSun, transform.position + new Vector3(0, 2, 0), Quaternion.identity);
+  }
+}
