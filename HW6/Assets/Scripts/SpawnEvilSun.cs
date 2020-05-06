c6e834167d4aff2ee20f90628136686e290f4960
Oleg <oanohin@outlook.com>
2020-05-06 20:37:36 +0400

    Fix Error 10856
    1) remove InvokeRepeating.
    2) private void Start().
    3)ren _timeStart to _numberOfSequenceOfStart.

--- HW6/Assets/Scripts/SpawnEvilSun.cs
+++ HW6/Assets/Scripts/SpawnEvilSun.cs
@@ -1,19 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class SpawnEvilSun : MonoBehaviour
 {
   [SerializeField] private EvilSun _evilSun = null;
-  [SerializeField] float _timeStart = 0;
+  [SerializeField] private int _numberOfSequenceOfStart = 1;
 
   private EvilSun _evilSunClone;
+  private float _timer = 0;
 
-  void Start()
+  private void Update()
   {
-    InvokeRepeating("GenerateEnemy", _timeStart, 2.0f);
+    if (_timer <= _numberOfSequenceOfStart)
+    {
+      _timer += Time.deltaTime;
+      if (_timer >= _numberOfSequenceOfStart)
+      {
+        StartCoroutine(GenerateEnemy());
+        _timer += Time.deltaTime;
+      }
+    }
   }
 
-  private void GenerateEnemy()
+  private IEnumerator GenerateEnemy()
   {
-    _evilSunClone = Instantiate(_evilSun, transform.position + new Vector3(0, 0, 0), Quaternion.identity);
+    while (true)
+    {
+      _evilSunClone = Instantiate(_evilSun, transform.position + new Vector3(0, 0, 0), Quaternion.identity);
+      yield return new WaitForSeconds(2f);
+    }
   }
 }
