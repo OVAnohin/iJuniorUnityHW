1da72a9dcb563e8ed351412e2d809d1d227d8ae0
Oleg <oanohin@outlook.com>
2020-05-14 22:32:47 +0400

    HW9 Создание плавно изменяемого индикатора здоровья
    Создание плавно изменяемого индикатора здоровья

---
+++ HW9/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+  [SerializeField] private Slider _slider;
+
+  public void OnButtonClickHeal()
+  {
+    _slider.value += 1;
+  }
+
+  public void OnButtonClickHit()
+  {
+    _slider.value -= 1;
+  }
+}
