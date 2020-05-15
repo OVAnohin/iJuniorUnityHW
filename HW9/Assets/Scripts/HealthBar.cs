409fef57c94a54213c550385f7c131ed95d6a045
Oleg <oanohin@outlook.com>
2020-05-15 20:06:00 +0400

    Fix Error 11361

--- HW9/Assets/Scripts/HealthBar.cs
+++ HW9/Assets/Scripts/HealthBar.cs
@@ -1,17 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-  [SerializeField] private Slider _slider;
+  [SerializeField] private Slider _slider = null;
+  [SerializeField] private float _duration = 1f;
 
   public void OnButtonClickHeal()
   {
-    _slider.value += 1;
+    StartCoroutine(ChangeLife(0.1f));
   }
 
   public void OnButtonClickHit()
   {
-    _slider.value -= 1;
+    StartCoroutine(ChangeLife(-0.1f));
+  }
+
+  private IEnumerator ChangeLife(float value)
+  {
+    float _min = _slider.value;
+    float _max = _min + value;
+    float runningTime = 0;
+    float normalizeValue = 0;
+    while (runningTime < _duration)
+    {
+      runningTime += Time.deltaTime;
+      normalizeValue = runningTime / _duration;
+      _slider.value = Mathf.Lerp(_min, _max, normalizeValue);
+
+      yield return null;
+    }
   }
 }
