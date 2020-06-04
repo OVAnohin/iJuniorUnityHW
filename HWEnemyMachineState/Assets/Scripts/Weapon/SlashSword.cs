7cfee4e7248498999847eb4b6a1aebc7a03f2128
Oleg Anohin <oanohin@outlook.com>
2020-06-04 23:07:56 +0400

    Add Knight Attack

---
+++ HWEnemyMachineState/Assets/Scripts/Weapon/SlashSword.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashSword : Ammo
+{
+  private float _dictanceCovered = 0;
+
+  private void Update()
+  {
+    if (_dictanceCovered > Distance)
+    {
+      Destroy(gameObject);
+    }
+
+    transform.Translate(Vector2.right * Speed * Time.deltaTime);
+    _dictanceCovered += Time.deltaTime;
+  }
+
+  private void OnTriggerEnter2D(Collider2D collider2D)
+  {
+    if (collider2D.GetComponent<Player>() != null)
+    {
+      collider2D.GetComponent<Player>().ApplyDamage(Damage);
+      Destroy(gameObject);
+    }
+  }
+}
