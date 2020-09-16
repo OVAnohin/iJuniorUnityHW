314aadeff5ab9c30c1319493e4fc52de0e501386
Oleg Anohin <oanohin@outlook.com>
2020-09-16 21:46:23 +0400

    Pick Up

--- Platformer2D/Assets/Scripts/Environement/BonusHeart/BonusHeart.cs
+++ Platformer2D/Assets/Scripts/Environement/BonusHeart/BonusHeart.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class BonusHeart : MonoBehaviour
 {
+  [SerializeField] private AudioClip _takeSound = default;
+
+  private AudioSource _audioSource;
+
+  private void Start()
+  {
+    _audioSource = GetComponent<AudioSource>();
+  }
+
   private void OnTriggerEnter2D(Collider2D collision)
   {
     if (collision.TryGetComponent<Player>(out Player player))
     {
       player.TakeBonusLive();
-      Destroy(gameObject);
+      _audioSource.clip = _takeSound;
+      _audioSource.Play();
+      Destroy(gameObject, 0.2f);
     }
   }
 }
--- Platformer2D/Assets/Scripts/Environement/HealthPotion/HealthPotion.cs
+++ Platformer2D/Assets/Scripts/Environement/HealthPotion/HealthPotion.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class HealthPotion : MonoBehaviour
 {
+  [SerializeField] private AudioClip _takeSound = default;
+
+  private AudioSource _audioSource;
+
+  private void Start()
+  {
+    _audioSource = GetComponent<AudioSource>();
+  }
+
   private void OnTriggerEnter2D(Collider2D collision)
   {
     if (collision.TryGetComponent<Player>(out Player player))
     {
       player.TakeHealthPotion();
-      Destroy(gameObject);
+      player.TakeBonusLive();
+      _audioSource.clip = _takeSound;
+      Destroy(gameObject, 0.2f);
     }
   }
 }
